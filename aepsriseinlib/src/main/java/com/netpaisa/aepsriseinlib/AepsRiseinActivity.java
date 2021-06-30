bcd2eda20a341edd691d60bbfddba6452043a148
Apple <apple@Apples-MacBook-Pro.local>
2021-06-30 16:44:23 +0530

    Initial commit aeps library

---
+++ aepsriseinlib/src/main/java/com/netpaisa/aepsriseinlib/AepsRiseinActivity.java
@@ -0,0 +1,14 @@
+package com.netpaisa.aepsriseinlib;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AepsRiseinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aeps_risein);
+    }
+}

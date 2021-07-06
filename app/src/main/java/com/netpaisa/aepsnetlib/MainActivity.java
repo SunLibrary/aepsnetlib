777dd5663609e4eed6e8320406b12af297e335c5
SuryaBhati <suryabhati203@gmail.com>
2021-07-06 13:53:09 +0530

    5th update

--- app/src/main/java/com/netpaisa/aepsnetlib/MainActivity.java
+++ app/src/main/java/com/netpaisa/aepsnetlib/MainActivity.java
@@ -2,7 +2,10 @@ package com.netpaisa.aepsnetlib;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.netpaisa.aepsriseinlib.AepsRiseinActivity;
 
 import java.util.Objects;
 
@@ -14,6 +17,20 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-
+               Intent intentIciciKyc = new Intent(MainActivity.this, AepsRiseinActivity.class);
+//                intentIciciKyc.putExtra("agent_id", AppConst.AGENT_OUTLET_ID);
+//                intentIciciKyc.putExtra("developer_id", AppConst.RISETECH_DEV_ID);
+//                intentIciciKyc.putExtra("password", AppConst.RISEINTECH_PASSWORD);
+//                intentIciciKyc.putExtra("mobile", mMobileNo);
+//                intentIciciKyc.putExtra("aadhaar", mAadhaarNo);
+//                intentIciciKyc.putExtra("email", mEmailId);
+//                intentIciciKyc.putExtra("pan", mPanNo);
+                intentIciciKyc.putExtra("primary_color", R.color.colorPrimary);
+                intentIciciKyc.putExtra("accent_color", R.color.colorAccent);
+                intentIciciKyc.putExtra("primary_dark_color", R.color.colorPrimaryDark);
+                intentIciciKyc.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                //startActivityForResult(intentIciciKyc, 1);
+                //startActivityForResult(intentIciciKyc, 1);
+                startActivity(intentIciciKyc);
     }
 }

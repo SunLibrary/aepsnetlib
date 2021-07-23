a585ab20191891f43e1362bffc54ab881628b6f1
SuryaBhati <suryabhati203@gmail.com>
2021-07-23 17:01:59 +0530

    15th update

--- app/src/main/java/com/netpaisa/aepsnetlib/MainActivity.java
+++ app/src/main/java/com/netpaisa/aepsnetlib/MainActivity.java
@@ -54,16 +54,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.idGoToAEPS:
 
 
-                Intent intentTrans = new Intent(MainActivity.this, AepsRiseinActivity.class);
-                intentTrans.putExtra("api_access_key", "2755870fa87c2aed97skg43fa9e49db1");
-                intentTrans.putExtra("outletid", "16476");
-                intentTrans.putExtra("pan_no", "AZRPG6750B");
-                intentTrans.putExtra("app_label", "Aeps 1");
-                intentTrans.putExtra("primary_color", R.color.colorPrimary);
-                intentTrans.putExtra("accent_color", R.color.colorAccent);
-                intentTrans.putExtra("primary_dark_color", R.color.colorPrimaryDark);
-                intentTrans.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(intentTrans);
+//                Intent intentTrans = new Intent(MainActivity.this, AepsRiseinActivity.class);
+//                intentTrans.putExtra("api_access_key", "2755870fa87c2aed97skg43fa9e49db1");
+//                intentTrans.putExtra("outletid", "16476");
+//                intentTrans.putExtra("pan_no", "AZRPG6750B");
+//                intentTrans.putExtra("app_label", "Aeps 1");
+//                intentTrans.putExtra("primary_color", R.color.colorPrimary);
+//                intentTrans.putExtra("accent_color", R.color.colorAccent);
+//                intentTrans.putExtra("primary_dark_color", R.color.colorPrimaryDark);
+//                intentTrans.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                startActivity(intentTrans);
 
                 break;
         }
